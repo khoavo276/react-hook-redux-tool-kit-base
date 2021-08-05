3d0d54f7900e05027b7d76ac07cc4dffe4b23e74
KHOA VO <khoava@concrete-corp.com>
2021-08-05 17:39:16 +0700

    Init icon

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
